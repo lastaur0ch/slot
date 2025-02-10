17516facb5c0081497b62fedd763f08808807a3a
lastaur0ch <joelxjohnson@gmail.com>
2025-02-10 22:02:53 +0100

    added NaN loop

--- project.js
+++ project.js
@@ -1,7 +1,18 @@
 const prompt = require("prompt-sync")();
 
 const deposit = () => {
-    const depositAmount = prompt("Enter a deposit amount: ");
+    while(true) {
+        const depositAmount = prompt("Enter a deposit amount: ");
+        const numberDepositAmount = parseFloat(depositAmount);
+
+        if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+            console.log("Invalid deposit amount, try again!")
+        } else {
+            return numberDepositAmount;
+        }
+    }
+    
 };
 
-deposit();
+const depositAmount = deposit();
+console.log(depositAmount);
