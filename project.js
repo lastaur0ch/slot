d3d441e774ca5ef720683e0588b2bad190b206fb
lastaur0ch <joelxjohnson@gmail.com>
2025-02-11 06:24:28 +0100

    get line number function

--- project.js
+++ project.js
@@ -14,5 +14,19 @@ const deposit = () => {
     
 };
 
+const getNumberOfLines = () => {
+    while(true) {
+        const lines = prompt("Enter the number of lines to bet on (1-3): ");
+        const numberOfLines = parseFloat(lines);
+
+        if(isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines >3) {
+            console.log("Invalid number of lines. Must be between 1 and 3. Try again!")
+        } else {
+            return numberOfLines;
+        }
+    }
+};
+
 const depositAmount = deposit();
-console.log(depositAmount);
+const numberOfLines = getNumberOfLines();
+// console.log(depositAmount);
