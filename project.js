4d9679a881b283906df9ab55ac39023d296c81ca
lastaur0ch <joelxjohnson@gmail.com>
2025-02-11 07:20:22 +0100

    fix global object

--- project.js
+++ project.js
@@ -4,17 +4,17 @@ const ROWS = 3;
 const COLS = 3;
 
 const SYMBOLS_COUNT = {
-    "A": 2,
-    "B": 4,
-    "C": 6,
-    "D": 8
+    A: 2,
+    B: 4,
+    C: 6,
+    D: 8
 }
 
 const SYMBOLS_VALUES = {
-    "A": 5,
-    "B": 4,
-    "C": 3,
-    "D": 2
+    A: 5,
+    B: 4,
+    C: 3,
+    D: 2
 }
 
 
