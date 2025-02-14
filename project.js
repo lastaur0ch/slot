4b575687b1bb53e5cdcbdac1e23edc74cdf4b319
lastaur0ch <joelxjohnson@gmail.com>
2025-02-14 08:36:19 +0100

    beautify row print

--- project.js
+++ project.js
@@ -94,10 +94,22 @@ const transpose = (reels) => {
     return rows;
 }
 
-// let balance = deposit();
-// const numberOfLines = getNumberOfLines();
-// const bet = getBet(balance, numberOfLines);
+const printRows = (rows) => {
+    for (const row of rows) {
+        let rowString = "";
+        for (const [i, symbol] of row.entries()) {
+            rowString += symbol
+            if (i != row.length - 1) {
+                rowString += " | ";
+            }
+        }
+        console.log(rowString);
+    }
+}
+
+let balance = deposit();
+const numberOfLines = getNumberOfLines();
+const bet = getBet(balance, numberOfLines);
 const reels = spin();
 const rows = transpose(reels);
-console.log(reels);
-console.log(rows);
+printRows(rows);
